dbb07ed34dda78c963e5d2299464f2abe7a61a33
woodsman1 <19ucc114@lnmiit.ac.in>
2021-08-16 16:28:24 +0530

    fixed count in 'create problem signal'

--- problem_manager_app/signals.py
+++ problem_manager_app/signals.py
@@ -14,13 +14,14 @@ def create_problem(sender, instance, created, **kwargs):
                 p = SolvedProblem.objects.create(linked_user=user, solved_count=0)
             p.solved_problems.add(problem)
             p.solved_count += 1
+            p.save()
         else:
             p = UnsolvedProblem.objects.get(linked_user=user)
             if p is None:
                 p = UnsolvedProblem.objects.create(linked_user=user, unsolved_count=0)
             p.unsolved_problems.add(problem)
             p.unsolved_count += 1       
-
+            p.save()
 
 @receiver(post_save, sender=Problem)
 def updated_problem(sender, instance, created, **kwargs):
