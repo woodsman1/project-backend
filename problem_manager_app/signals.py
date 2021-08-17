056a55c3f97cc69dfe46ab36bb199c8c1d6ad2af
woodsman1 <19ucc114@lnmiit.ac.in>
2021-08-17 17:02:14 +0530

    fixed signal

--- problem_manager_app/signals.py
+++ problem_manager_app/signals.py
@@ -30,21 +30,30 @@ def updated_problem(sender, instance, created, **kwargs):
         user = problem.linked_user
         p = None
         if(problem.solved):
-            p = SolvedProblem.objects.get(linked_user=user)
-            up = UnsolvedProblem.objects.get(linked_user=user)
+            p = SolvedProblem.objects.get(linked_user=user)            
             if p is None:
                 p = SolvedProblem.objects.create(linked_user=user, solved_count=0)
+            
+            if len(p.solved_problems.filter(id=problem.id)):  # if problem already exist correct place no changes needed 
+                return
+
+            up = UnsolvedProblem.objects.get(linked_user=user)
             up.unsolved_problems.remove(problem)
             up.unsolved_count -= 1
             up.save()
+            
             p.solved_problems.add(problem)
             p.solved_count += 1
             p.save()
         else:
             p = UnsolvedProblem.objects.get(linked_user=user)
-            sp = SolvedProblem.objects.get(linked_user=user)
             if p is None:
                 p = UnsolvedProblem.objects.create(linked_user=user, unsolved_count=0)
+
+            if len(p.unsolved_problems.filter(id=problem.id)):  # if problem already exist correct place no changes needed 
+                return
+
+            sp = SolvedProblem.objects.get(linked_user=user)
             sp.solved_problems.remove(problem)
             sp.solved_count -=1
             sp.save()
